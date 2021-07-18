8bacbc72e7ca9f50c7cd4e623313eb4a8a002d88
Lasse Siebert <lasse@wakeword.de>
2021-07-18 10:24:58 +0200

    Typescript initial build

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getPlural = void 0;
+var pluralize = require("pluralize");
+/**
+* @Method: Returns the plural form of any noun.
+* @Param {string}
+* @Return {string}
+*/
+function getPlural(str) {
+    return pluralize.plural(str);
+}
+exports.getPlural = getPlural;
